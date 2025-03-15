674f49f09982d5dcb6154bbf6b6b13dc2574fe14
Nogeese <leon8326nogeese@outlook.com>
2025-03-15 16:38:45 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,25 @@ SOURCEPOD64=$(pwd)
 cd $SOURCEPOD64/build/essential/script.sh.d/
 echo "export arch=pod64-docker" | sudo tee -a /etc/environment
 export arch=pod64-docker
-echo "curl https://raw.githubusercontent.com/nogeese-org/mirror/main/nogeese/os/$arch/setup.sh | sudo bash" >> pacman.sh
-echo "curl https://raw.githubusercontent.com/nogeese-org/mirror/main/bit/os/$arch/setup.sh | sudo bash" >> pacman.sh
-bash pacman.sh
+cd /etc
+{
+    echo ""
+    echo "[bit]"
+    echo "Server = https://raw.githubusercontent.com/nogeese-org/mirror/stable/bit/os/pod64-docker"
+    echo "SigLevel = Optional TrustAll"
+} | sudo tee -a pacman.conf
+{
+    echo ""
+    echo "[nogeese]"
+    echo "Server = https://raw.githubusercontent.com/nogeese-org/mirror/main/bit/os/pod64-docker"
+    echo "SigLevel = Optional TrustAll"
+} | sudo tee -a pacman.conf
 cd $SOURCEPOD64/build
 mkdir appsrc
 cd appsrc
 curl -sSL https://raw.githubusercontent.com/nogeese-org/InfoKit/main/installer/install.sh | sudo bash
 cd ../sys
-git clone https://github.com/nogeese-org/stable.git ./master
+git clone https://github.com/nogeese-org/rolling.git ./master
 cd master
 bash architectures/pod64-docker.sh
 bash scripts/appsetup.sh
@@ -22,7 +32,7 @@ git clone https://github.com/nogeese-org/podutils-docker.git ./podutils
 cd podutils
 bash scripts/flush.sh
 rm -rf /etc/os-release
-curl https://nogeese-org.github.io/pod64-stable-docker/os-release -o /etc/os-release
+curl https://nogeese-org.github.io/pod64-rolling-docker/os-release -o /etc/os-release
 clear
 echo "This message was automaticly sent."
 echo "The system is ready to use."
